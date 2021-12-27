066bae582eace051bdf686762fe92c07b1984695
sabil62 <shresthasabil62@gmail.com>
2021-12-27 14:04:14 +0545

    box class complete

--- Javascript/assignment 3/javascript/main.js
+++ Javascript/assignment 3/javascript/main.js
@@ -3,11 +3,12 @@ class Ball {
     this.mainClass = mainClass;
     this.diameter = diameter;
     this.canvasWidth = canvasWidth;
+    this.ballElement;
     this.x = 0;
     this.y = 0;
     this.dirX = 0;
     this.dirY = 0;
-    this.backgroundColor = null;
+    this.backgroundColor;
   }
   setPosition(x, y) {
     this.x = x;
@@ -42,5 +43,36 @@ class Ball {
     }
   }
   //if collide then change box1.changeBall(box2)
-  changeBallDir() {}
+  changeBallDir(collidingBall) {
+    let dirXstore = this.dirX;
+    this.dirX = collidingBall.dirX;
+    collidingBall.dirX = dirXstore;
+
+    let dirYstore = this.dirY;
+    this.dirY = collidingBall.dirY;
+    collidingBall.dirY = dirYstore;
+
+    this.move();
+    collidingBall.move();
+  }
+  createBall() {
+    this.ballElement = document.createElement("div");
+    this.ballElement.style.position = "absolute";
+    this.ballElement.style.borderRadius = "50%";
+    this.ballElement.style.width = this.diameter + "px";
+    this.ballElement.style.height = this.diameter + "px";
+    this.ballElement.style.border = "1px solid rgb(160,160,160)";
+    this.ballElement.style.backgroundColor = this.backgroundColor;
+    this.mainClass.appendChild(this.ballElement);
+  }
+
+  move() {
+    this.x = this.x + this.dirX;
+    this.y = this.y + this.dirY;
+    this.illustrate();
+  }
+  illustrate() {
+    this.ballElement.style.top = this.dirY + "px";
+    this.ballElement.style.left = this.dirX + "px";
+  }
 }
